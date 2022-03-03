e58ad30c8f8c1888b0a3ec5c33fdc2f76ab5e331
Roger Woo <rogerywoo@gmail.com>
2022-03-02 20:55:53 -0500

    Initial commit

---
+++ src/test/java/com/rwoo/springboot/basics/BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwoo.springboot.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
